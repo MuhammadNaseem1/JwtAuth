427eb57d3ea5fbfe9062de0c02e8806737859a51
Muhammad.Naseem <muhammad.naseem@alhabibfunds.com>
2025-05-07 17:04:51 +0500

    Jwt token authentication implemented

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,14 +1,24 @@
 const jwt = require('jsonwebtoken');
+
 function verifyToken(req, res, next) {
-const token = req.header('Authorization');
-if (!token) return res.status(401).json({ error: 'Access denied' });
-try {
- const decoded = jwt.verify(token, 'pakistan');
- req.userId = 1;
- next();
- } catch (error) {
- res.status(401).json({ error: 'Invalid token' });
- }
- };
+  const authHeader = req.header('Authorization');
+  console.log("authHeader", authHeader);
+
+  // Ensure it starts with Bearer and extract the token
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Access denied. No token provided.' });
+  }
+
+  const token = authHeader.split(' ')[1]; // Get just the token
+  try {
+    const decoded = jwt.verify(token, 'pakistan');
+    console.log("decoded", decoded);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    console.log("error", error);
+    res.status(401).json({ error: 'Invalid token' });
+  }
+}
 
 module.exports = verifyToken;
